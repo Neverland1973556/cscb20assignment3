d87aa72c93408daef6b20c749d7a003d02db0c7c
Felix-Xu-Vanish <2556692804@qq.com>
2022-04-02 15:03:29 -0400

    database setup

--- app.py
+++ app.py
@@ -5,9 +5,69 @@ from flask_bcrypt import Bcrypt
 from sqlalchemy import text # textual queries
 
 app = Flask(__name__)
+# what is this?
+app.config['SECRET_KEY'] = 'secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///a3database.db'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes = 15)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
+# todo: change to our own type
+class Teacher(db.Model):
+    __tablename__ = 'Person'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique=True, nullable = False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable = False)
+    notes = db.relationship('Notes', backref='author', lazy = True)
+
+    def __repr__(self):
+        return f"Person('{self.username}', '{self.email}')"
+
+class Student(db.Model):
+    __tablename__ = 'Student'
+    stuId = db.Column(db.Integer, primary_key = True)
+    stuName = db.Column(db.String(20), nullable = False)
+    username = db.Column(db.String(20), nullable = False, unique=True)
+    password = db.Column(db.String(20), nullable = False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+
+    # foreign key, need to change
+    person_id = db.Column(db.Integer, db.ForeignKey('Person.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Student('{self.stuName}', '{self.email}')"
+
+class Evaluation(db.Model):
+    __tablename__ = 'Evaluation'
+    eid = db.Column(db.Integer, primary_key = True)
+    typeName = db.Column(db.String(20), nullable = False, unique=True)
+    totalMark = db.Column(db.Integer)
+    stuMark = db.Column(db.Integer)
+    remarkReq = db.Column(db.Boolean)
+    remarkText = db.Column(db.String(200), nullable=False)
+
+    # foreign key, need to change
+    student_id = db.Column(db.Integer, db.ForeignKey('Student.stuId'), nullable = False)
+    instructor_id = db.Column(db.Integer, db.ForeignKey('Instructor.stuId'), nullable = False)
+
+    def __repr__(self):
+        return f"Evaluation('{self.typeName}', '{self.stuMark}')"
+
+class Feedback(db.Model):
+    __tablename__ = 'Feedback'
+    fid = db.Column(db.Integer, primary_key = True)
+    feedbackText = db.Column(db.String(200), nullable=False)
+
+    # foreign key, need to change
+    student_id = db.Column(db.Integer, db.ForeignKey('Student.stuId'), nullable = False)
+    instructor_id = db.Column(db.Integer, db.ForeignKey('Instructor.stuId'), nullable = False)
+
+    def __repr__(self):
+        return f"Evaluation('{self.student_id}', '{self.instructor_id}')"
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("index.html")
 
